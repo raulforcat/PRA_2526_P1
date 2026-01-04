bf94aece96c08b0a83255c9aa7f7d591ee9467bb
raulforcat <raulforcat@gmail.com>
2026-01-04 20:49:37 +0100

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -11,25 +11,50 @@ class ListArray : public List<T> {
 	    static const int MINSIZE = 2;
 
 	    void resize(int new_size){
-		    T* new_arr = new [new_size];
+		    T* new_arr = new T[new_size];
 		    for(int i = 0; i < n; i++){
 			    new_arr[i] = arr[i];
 		    }
-		    delete[arr];
+		    delete[] arr;
 		    arr = new_arr;
 		    max = new_size;
 	    }
 
     public:
-	    ListArray();
-	    ~ListArray();
+	    ListArray() {
+        arr = new T[MINSIZE];
+        max = MINSIZE;
+        n = 0;
+      }
 
-	    T operator[](int pos);
-	    friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
+	    ~ListArray() {
+        delete[] arr;
+      }
+
+	    T operator[](int pos){
+         if (pos < 0 || pos >= n){
+			    throw std::out_of_range("Posición inválida");
+		    }
+		    return arr[pos];
+      }
+
+	    friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+        out << "[";
+
+        for (int i = 0; i < list.n; i++) {
+          out << list.arr[i];
+            if (i < list.n - 1){
+              out << ", ";
+            }
+        }
+
+        out << "]";
+        return out;
+      }
 
 	    void insert(int pos, T e) override {
 		    if (pos < 0 || pos > n){
-			    throw std::out_of_range("Posición inválida en insert()");
+			    throw std::out_of_range("Posición inválida");
 		    }
 		    if (n == max){
 			    resize(max * 2);
@@ -47,4 +72,45 @@ class ListArray : public List<T> {
 	    void prepend(T e) override {
 		    insert(0, e);
 	    }
+      
+      T remove(int pos) override{
+        if (pos < 0 || pos >= n){
+			    throw std::out_of_range("Posición inválida");
+		    }
+
+        T elem_ret = arr[pos];
+
+        for (int i = pos; i < n-1; i++){
+          arr[i] = arr[i+1];
+        }
+
+        //arr[n-1] = NULL;
+        n--;
+        return elem_ret;
+      }
+
+      T get(int pos) override{
+        if (pos < 0 || pos >= n){
+			    throw std::out_of_range("Posición inválida");
+		    }
+        return arr[pos];
+      }
+
+      int search(T e) override{
+        for (int i = 0; i < n; i++){
+          if (arr[i] == e){
+            return i;
+          }
+        }
+        return -1;
+      }
+
+      bool empty() override{
+        return n == 0;
+      }
+
+      int size() override{
+        return n;
+      }
+
 };
