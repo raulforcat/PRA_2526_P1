45cfdbb6be7ad57b0c298b176da4ba19c7b84b9c
raulforcat <raulforcat@gmail.com>
2026-01-05 00:28:00 +0100

    Añadida implementación de la clase Point2D con su respectivo test; Makefile actualizado

--- Makefile
+++ Makefile
@@ -12,3 +12,11 @@ bin/testNode: testNode.cpp Node.h
 bin/testListLinked: testListLinked.cpp ListLinked.h List.h
 	mkdir -p bin
 	g++ -o bin/testListLinked testListLinked.cpp
+
+Point2D.o: Point2D.h Point2D.cpp
+	g++ -c Point2D.cpp
+
+bin/testPoint2D: testPoint2D.cpp Point2D.o
+	g++ -c testPoint2D.cpp
+	mkdir -p bin
+	g++ -o bin/testPoint2D testPoint2D.o Point2D.o
---
+++ Point2D.cpp
@@ -0,0 +1,13 @@
+#include "Point2D.h"
+double Point2D::distance(const Point2D &a, const Point2D &b){
+	return std::sqrt(std::pow(a.x - b.x, 2)+ std::pow(a.y - b.y, 2));
+}
+bool operator==(const Point2D &a, const Point2D &b){
+	return (a.x == b.x && a.y == b.y);
+}
+bool operator!=(const Point2D &a, const Point2D &b){
+	return (a.x != b.x || a.y != b.y);
+}
+std::ostream& operator<<(std::ostream &out, const Point2D &p){
+	return out << "(" << p.x << ", " << p.y << ")";
+}
