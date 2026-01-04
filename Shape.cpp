fe8ed01095d21ff941dee3d4c51af16348075d3b
raulforcat <raulforcat@gmail.com>
2026-01-05 00:41:48 +0100

    Añadida la clase Shape; Makefile actualizado

---
+++ Shape.cpp
@@ -0,0 +1,20 @@
+#include "Shape.h"
+
+Shape::Shape(std::string color){
+	if (color != "red" || color != "green" || color != "blue"){
+		throw std::invalid_argument("Color no válido");
+        }
+        this -> color = color;
+}
+
+std::string Shape::get_color() const{
+	return color;
+}
+
+void Shape::set_color(std::string c){
+	if (c != "red" || c != "green" || c != "blue"){
+		throw std::invalid_argument("Color no válido");
+        }
+        color = c;
+}
+
