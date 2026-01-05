67c91892f5b0807337bea7d97caad30018fc417d
raulforcat <raulforcat@gmail.com>
2026-01-05 01:46:42 +0100

    Shape.cpp actualizado

--- Shape.cpp
+++ Shape.cpp
@@ -1,7 +1,7 @@
 #include "Shape.h"
 
 Shape::Shape(std::string color){
-	if (color != "red" || color != "green" || color != "blue"){
+	if (color != "red" && color != "green" && color != "blue"){
 		throw std::invalid_argument("Color no válido");
         }
         this -> color = color;
@@ -12,7 +12,7 @@ std::string Shape::get_color() const{
 }
 
 void Shape::set_color(std::string c){
-	if (c != "red" || c != "green" || c != "blue"){
+	if (c != "red" && c != "green" && c != "blue"){
 		throw std::invalid_argument("Color no válido");
         }
         color = c;
