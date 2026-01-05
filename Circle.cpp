bfcdf1f0e1595ae8288bb868ed131e18b4a4cfc2
raulforcat <raulforcat@gmail.com>
2026-01-05 01:47:52 +0100

    Añadida la clase Circle con su respectivo test; Makefile actualizado

---
+++ Circle.cpp
@@ -0,0 +1,45 @@
+#include "Circle.h"
+
+static const double pi = 3.1416;
+
+Circle::Circle()
+    : Shape("red"), center(), radius(1) {}
+
+Circle::Circle(std::string color, Point2D center, double radius)
+    : Shape(color), center(center), radius(radius) {}
+
+Point2D Circle::get_center() const{
+  return center;
+}
+
+void Circle::set_center(Point2D p){
+  center = p;
+}
+
+double Circle::get_radius() const{
+  return radius;
+}
+
+void Circle::set_radius(double r){
+  radius = r;
+}
+
+std::ostream& operator<<(std::ostream &out, const Circle &c){
+  return out << "(Color: " <<  c.get_color() << "; Centro: " << c.get_center() << "; Radio: " << c.get_radius() << ")";
+}
+
+double Circle::area() const{
+  return pi * radius * radius;
+}
+
+double Circle::perimeter() const{
+  return pi * radius * 2;
+}
+
+void Circle::translate(double incX, double incY){
+  center = Point2D(center.x + incX, center.y + incY);
+}
+
+void Circle::print(){
+  std::cout << "Color: " << get_color() << " Centro: " << get_center() << " Radio: " << get_radius();
+}
--- Makefile
+++ Makefile
@@ -23,3 +23,11 @@ bin/testPoint2D: testPoint2D.cpp Point2D.o
 
 Shape.o: Shape.h Shape.cpp
 	g++ -c Shape.cpp
+
+Circle.o: Circle.h Circle.cpp
+	g++ -c Circle.cpp
+
+bin/testCircle: testCircle.cpp Circle.o Shape.o Point2D.o
+	g++ -c testCircle.cpp
+	mkdir -p bin
+	g++ -o bin/testCircle testCircle.o Circle.o Shape.o Point2D.o
